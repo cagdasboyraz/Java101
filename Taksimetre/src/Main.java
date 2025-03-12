8c43c76f8040670564f80aecf92b0eca280fc729
Çağdaş Boyraz <135648166+cagdasboyraz@users.noreply.github.com>
2025-03-12 12:55:34 +0300

    Taksimetre
    Taksımetre Hesaplama

---
+++ Taksimetre/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("--------------");
+        System.out.println("Taksimetre Tutarını Hesaplama");
+        System.out.println("--------------");
+        double toplamTutar = 0, mesafe = 0, km = 2.2, ilkTutar = 10;
+        System.out.print("Mesafeyi km cinsinden giriniz: ");
+        mesafe = input.nextDouble();
+        input.close();
+        toplamTutar = ilkTutar + mesafe * km;
+        toplamTutar= (toplamTutar < 20) ? 20 : toplamTutar;
+        System.out.println("--------------");
+        System.out.format("Toplam Tutar: %.2f TL\n", toplamTutar);
+    }
+}
