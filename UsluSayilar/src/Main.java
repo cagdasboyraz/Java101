255dbb588aa4b96d8a8fac8ce24f28239863e65b
Çağdaş Boyraz <135648166+cagdasboyraz@users.noreply.github.com>
2025-03-14 14:18:22 +0300

    Üslü Sayıları Bulma
    Üslü sayı hesaplayan program

---
+++ UsluSayilar/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int n, k;
+        int total = 1;
+
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Ussu alinacak sayi");
+        n = input.nextInt();
+        System.out.println("ust olucak sayi");
+        k = input.nextInt();
+
+
+
+        for (int i = 1; i <=k ; i++){
+            total *= n;
+        }
+
+        System.out.println(total);
+    }
+}
